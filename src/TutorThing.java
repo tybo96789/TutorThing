fad4bd4f7414ef50a3622a14b120452f9734f6a6
Tyler A <tyler.atiburcio@gmail.com>
2015-01-30 19:28:30 -1000

    Append to last
    Append to last

--- src/TutorThing.java
+++ src/TutorThing.java
@@ -1,5 +1,14 @@
 
+import java.awt.Container;
+import java.awt.Dimension;
+import java.awt.Toolkit;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
+import java.util.ArrayList;
+import java.util.Date;
 import javax.swing.JFrame;
+import static javax.swing.JFrame.EXIT_ON_CLOSE;
+import javax.swing.JPanel;
 
 /*
  * To change this license header, choose License Headers in Project Properties.
@@ -21,9 +30,90 @@ public class TutorThing {
     }
 
     private static class TutorManagement  extends JFrame{
+        
+        
+        //Frame Stuff
+        private final String TITLE = "Tutoring Management Program";
+        private final int INITAL_WIDTH = 700, INITAL_HEIGHT = 700;
+        
+        //Panel Stuff
+        private ArrayList<Container> containers = new ArrayList<Container>();
+        private JPanel panel = new JPanel();
+        
+        //Saving stuff
+        private final String FILE_EXTENSION = ".CSV";
 
         public TutorManagement() {
             
+            //Frame stuff
+            this.setTitle(TITLE);
+            this.setSize(INITAL_WIDTH, INITAL_HEIGHT);
+            this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+            this.addWindowListener(new CloseWindowListener());
+
+            //Used to center the Window to the center of the screen no matter what computer you are using
+            Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
+            this.setMaximizedBounds(null);
+            this.setLocation(dim.width / 2 - this.getSize().width / 2, dim.height / 2 - this.getSize().height / 2);
+
+            //To adjust the size of the text area when the frame size is adjusted
+            //this.addComponentListener(new JFrameComponentAdaptor());            
+
+            //Container stuff
+            this.makeContainers();
+            for (Container c : this.containers) {
+                this.panel.add(c);
+            }
+            this.add(this.panel);
+
+            //make it visable to the user
+            this.setVisible(true);
+            
+        }
+
+        private void makeContainers() {
+            throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        }
+    }
+
+    private static class CloseWindowListener implements WindowListener {
+
+        public CloseWindowListener() {
+        }
+
+        @Override
+        public void windowOpened(WindowEvent we) {
+            throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        }
+
+        @Override
+        public void windowClosing(WindowEvent we) {
+            throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        }
+
+        @Override
+        public void windowClosed(WindowEvent we) {
+            throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        }
+
+        @Override
+        public void windowIconified(WindowEvent we) {
+            throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        }
+
+        @Override
+        public void windowDeiconified(WindowEvent we) {
+            throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        }
+
+        @Override
+        public void windowActivated(WindowEvent we) {
+            throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        }
+
+        @Override
+        public void windowDeactivated(WindowEvent we) {
+            throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
         }
     }
 
