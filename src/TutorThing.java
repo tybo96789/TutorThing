aadf9915452b02b097ed04aa2a2298acbd735393
Tyler A <tyler.atiburcio@gmail.com>
2015-01-30 19:08:42 -1000

    Code Cleanup
    Code Clean of Session object

--- src/TutorThing.java
+++ src/TutorThing.java
@@ -10,49 +10,41 @@
  */
 public class TutorThing {
 
-	/**
- 	* @param args the command line arguments
- 	*/
-	public static void main(String[] args) {
-    	Session s = new Session(System.nanoTime());
-   	 
-    	System.out.println("start = " + s.start + "\ndifference = " + s.calcTime());
-   	 
-	}
-   	 
-    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Session s = new Session(System.nanoTime());
+
+        System.out.println("start = " + s.start + "\ndifference = " + s.calcTime());
+
+    }
+
 }
 
+class Session {
 
+    long start, end;
+    String fName, lName, iD, course, instructor, tutor;
 
+    public Session(Long start, String fName, String lName, String iD, String course, String instructor, String tutor) {
+        this.start = start;
+        this.fName = fName;
+        this.lName = lName;
+        this.iD = iD;
+        this.course = course;
+        this.instructor = instructor;
+        this.tutor = tutor;
+    }
 
-class Session{
-	double adjustment = (Math.pow(10,9));
-	long a = (long)adjustment;
-	Long start, end;
-	String f, l, i, c, t;
-    
-	Session(String fname, String lname, String id, String c, String i, String tu, Long start){
-    	f = fname;
-    	l = lname;
-    	i = id;
-    	this.c = c;
-    	this.i = i;
-    	t = tu;
-   	 
-    	this.start = start;
-	}
-    
-    
-    
-	public Long calcTime(){
-   	 
-    	Long e = System.nanoTime();
-    	System.out.println("end = "+ e );
-    	System.out.println("a = " + a);
-    	return ((e - start))/a;
-           	 
-   	 
-	}
-    
+    public long calcTime() {
+        /*
+         Long e = System.nanoTime();
+         System.out.println("end = "+ e );
+         System.out.println("a = " + a);
+         return ((e - start))/a;
+         */
+        return (System.nanoTime() - start) / ((long) Math.pow(10, 9));
+    }
+
 }
